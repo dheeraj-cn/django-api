80194c749711d713391423c1fabb470caa412649
Dheeraj <dhirajyadav135@gmail.com>
2020-04-22 01:37:41 +0530

    Add Dockerfile

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4q8x7zk*3t$3ypeh%6_lt=&-p882y4p-$%m-*2w)ny*lbgppz*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
